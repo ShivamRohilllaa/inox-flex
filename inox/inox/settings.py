eb3f95e243e2fe8faeaf9e0290d5efbc42b4b630
Shivam Rohilla <shivamrohilla@Shivams-MacBook-Air.local>
2025-11-19 00:13:37 +0530

    changes

--- inox/inox/settings.py
+++ inox/inox/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8ytg84s0od2fyydimtm@%oc-@72od0h8g6-tizk-#z4an+1k78
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "inoxflex.co.in", "www.inoxflex.co.in"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Must be after SecurityMiddleware, before other middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,9 +122,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# WhiteNoise configuration for production static file serving
+# This allows Django to serve static files efficiently in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# WhiteNoise settings
+WHITENOISE_USE_FINDERS = True  # Use Django's staticfiles finders
+WHITENOISE_AUTOREFRESH = True  # Auto-refresh static files in development
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- inox/inox/urls.py
+++ inox/inox/urls.py
@@ -46,6 +46,8 @@ admin.site.site_header = "Inox Flex Admin"
 admin.site.site_title = "Inox Flex Admin Portal"
 admin.site.index_title = "Welcome to Inox Flex Admin Portal"
 
-#static files urls 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Static files URLs - only serve in development (DEBUG=True)
+# In production, use WhiteNoise middleware or configure your web server (nginx/Apache) to serve static files
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
